70499b53a119109aa306dff3a01c1c63244e994c
Apprentice <apprentice@dbc06.local>
2013-06-21 11:59:34 -0700

    created tasks migation and logic for ARGVs.  'complete' still not finished

--- todo.rb
+++ todo.rb
@@ -1,3 +1,21 @@
 require_relative 'config/application'
+require 'date'
 
-puts "Put your application code in #{File.expand_path(__FILE__)}"
+  case ARGV[0]
+  when 'list' then Task.all.print_list
+  when 'add'  then Task.create!(description: ARGV[1..-1])
+  when 'delete' then Task.delete(ARGV[1..-1])
+  when 'complete' then Task.update_attributes(completed_at: Time.now)
+  end
+
+
+module View
+  def print_list
+    
+  end
+
+  def feedback
+    
+  end
+
+end
